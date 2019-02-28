d0b281d72e93bb8314193bbff22392c6410c8b69
Lars Nihlmark <larsnihlmark@gmail.com>
2019-02-28 13:53:21 +0100

    Input for register and script with POST function

---
+++ Register/Register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+    <script src="RegisterJS.js" type="text/javascript"></script>
+    <title>Register</title>
+</head>
+<body>
+<!-- <form id="register" action="register.php" method="post" accept-charset='UTF-8'> -->
+<!-- <fieldset> -->
+<!-- <input type="hidden" name="submitted" id="submitted" value="1"/> -->
+
+<label for="email"> Email Adress:</label>
+<input type="text" name="email" id="email" maxlength="50"/>
+
+<label for="password"> Password:</label>
+<input type="password" name="password" id="password" maxlenght="50"/>
+<input type="submit" name="Submit" value="Submit" id="Button" />
+
+<!-- </fieldset> -->
+<!-- </form> -->
+<?php
+?>
+</body>
+</html>
