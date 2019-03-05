05b708752b5d679f724ec0dbd231fb94ccd2429a
Anton <antonwelliver27@gmail.com>
2019-03-05 14:01:11 +0100

    added classes and fixed bg bug

--- admin.php
+++ admin.php
@@ -15,54 +15,58 @@
     <script src="scripts/product.js"></script>
     <title>Admin page</title>
 </head>
-<body>
-<div class="body-container">
-
+<body class="body-container">
     <div class="ui middle aligned center aligned grid">
         <div class="column">
             <h2 class="ui red header">
-      <div class="content">
-        DALAMAT
-      </div>
-    </h2>
-    <form ui large form register-form>
-<div class="ui form" >
-    <div class="field">
-    <button class="ui button" id="updateProduct">Uppdatera antal produkter i lager</button>
-    <label for=""> Antal produkter:</label>
-    <input type="text" name="" id="" maxlength="50"/>
-    </div>
+            <div class="content">
+            DALAMAT
+            </div>
+        
+            </h2>
+            <form ui large form register-form>
+            <div class="ui form" >
+                <div class="field">
+                <label for="productID"> Produkt ID:</label>
+                <input type="text" name="productID" id="productID" maxlength="50"/>
+                <label for="sumProducts"> Antal produkter:</label>
+                <input type="text" name="sumProducts" id="sumProducts" maxlength="50"/>
 
-    <div class="field">
-    <button class="ui button" id="addProduct">Lägg till/Ta bort produkter</button>
-    <label for=""> Lägg till produktnamn:</label>
-    <input type="text" name="" id="" maxlength="50"/>
-    </div>
+                <button class="ui button" id="updateProduct">Uppdatera antal produkter i lager</button>
+                </div>
 
-    <div class="field">
-    <label for=""> Bild URL:</label>
-    <input type="text" name="" id="" maxlength="50"/>
-    </div>
+                <div class="field">
+                <label for="productName"> Lägg till produktnamn:</label>
+                <input type="text" name="productName" id="productName" maxlength="50"/>
+                </div>
 
-    <div class="field">
-    <label for=""> Produkt pris:</label>
-    <input type="text" name="" id="" maxlength="50"/>
-    </div>
+                <div class="field">
+                <label for="addImage"> Bild URL:</label>
+                <input type="text" name="addImage" id="addImage" maxlength="50"/>
+                </div>
 
-    <div class="field">
-    <label for=""> Produktkategori:</label>
-    <input type="text" name="" id="" maxlength="50"/>
-    </div>
+                <div class="field">
+                <label for="productPrice"> Produkt pris:</label>
+                <input type="text" name="productPrice" id="productPrice" maxlength="50"/>
+                </div>
 
-    <div>
-    <button class="ui button" id="seeOrders">Se gjorda beställningar</button>
-    </div>
+                <div class="field">
+                <label for="productCategory"> Produktkategori:</label>
+                <input type="text" name="productCategory" id="productCategory" maxlength="50"/>
 
-    <div>
-    <button class="ui button" id="newsletterList">Lista över nyhetsbrev</button>
+                <button class="ui button" id="addProduct">Lägg till/Ta bort produkter</button>
+                </div>
+
+                <div>
+                <button class="ui button" id="seeOrders">Se gjorda beställningar</button>
+                </div>
+
+                <div>
+                <button class="ui button" id="newsletterList">Lista över nyhetsbrev</button>
+                </div>
+                </form>
+            </div>
+        </div>
     </div>
-    </form>
-</div>
-</div>
 </body>
 </html>
