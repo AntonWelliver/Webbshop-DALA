1e36ccf660a5aca062a2e8ff376a110f39c52209
Anton <antonwelliver27@gmail.com>
2019-03-05 10:19:13 +0100

    added input and buttons

---
+++ admin.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin page</title>
+</head>
+<body>
+    
+    <label for="updateProduct"> Uppdatera antal produkter i lager:</label>
+    <input type="text" name="updateProduct" id="updateProduct" maxlength="50"/>
+
+    <button id="seeOrders">Se gjorda beställningar</button>
+
+    <button id="newsletterList">Lista över nyhetsbrev</button>
+
+</body>
+</html>
