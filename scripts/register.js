6cb3efc38ca59a5ac88dcf4ec6756c0635785fc4
alcr33k <alex.alle@hotmail.com>
2019-03-06 16:30:30 +0100

    Man kan nu registrera sig igen! Fixes#72

--- scripts/register.js
+++ scripts/register.js
@@ -1,12 +1,12 @@
 function registerUser(event, fields) {
-    console.log("heeeej", fields.email, fields.password);
     $.ajax({   
+        async : false,
         type: "POST",
         url: "api/handlers/userHandler.php",
         data:{action: "registerUser", email: fields.email, password: fields.password},
         success: function(data){ 
             if(data.status == 'error'){
-                alert("Error!");
+                alert(data.message);
             }else {
                 alert("Du har skapat konto");
             }
