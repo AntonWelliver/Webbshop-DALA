7a759edd77a00fa97f93da522d0e523cd3a340ca
Lars Nihlmark <larsnihlmark@gmail.com>
2019-03-07 13:16:29 +0100

    Added saving a username in both user and news

--- scripts/newsLetter.js
+++ scripts/newsLetter.js
@@ -2,17 +2,18 @@
 /* Function for check in newsHandler for matching */
 $(document).ready(function(){
     $("#newsReg").click(function(){
+        var username = $("#username").val();
         var name = $("#emailNews").val();
         console.log(name);
         $.ajax({   
             type: "POST",
             url: "api/handlers/newsHandler.php",
-            data:{action: "registera", email: name},
+            data:{action: "registera", email: name, username: username},
             success: function(data){ 
                 if(data == 'error'){
                     alert("Error!");
                 }else {
-                    alert(data);
+                    alert("Du är registerad för nyhetsbrev");
                 }
             }
         });
