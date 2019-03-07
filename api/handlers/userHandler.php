54e0936a3b1a75a6fd96198e87e9ccbc35e18c6b
alcr33k <alex.alle@hotmail.com>
2019-03-07 09:20:46 +0100

    Kan nu lägga till bild i databasen

--- api/handlers/userHandler.php
+++ api/handlers/userHandler.php
@@ -5,7 +5,7 @@ try {
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $email = $_POST['email'];
         $pass = $_POST['password'];
-        $user = new User($email, $pass);
+        $user = new User();
 
         if($_POST["action"] == "registerUser") {
             $user->register();
