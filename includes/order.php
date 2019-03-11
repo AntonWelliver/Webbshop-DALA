fbcb1360f965663f776152e6ffbb5612fa078ddd
alcr33k <alex.alle@hotmail.com>
2019-03-11 10:56:14 +0100

    OrderHistory bug-fix

--- includes/order.php
+++ includes/order.php
@@ -2,24 +2,6 @@
 require_once('../../includes/database.php');
 
 class Order{
-    private $connection;
-    private $database;
-
-    function __construct() {
-        $this->database = new Database();
-        $this->connection = $this->database->connect();
-    }
-    function orderHistory() {
-        $sql = "SELECT * FROM orderhistory";
-        $statement = $this->connection->prepare($sql);
-        $statement->execute();
-        $res = $statement->fetchAll();
-        /* $res = $statement->fetch(PDO::FETCH_OBJ); */
-        return $res;
-    }
-
-
-class Order{
     private $shipping;
     private $price;
     private $connection;
@@ -37,7 +19,14 @@ class Order{
         return $res;
     }
 
-
+    function orderHistory() {
+        $sql = "SELECT * FROM orderhistory";
+        $statement = $this->connection->prepare($sql);
+        $statement->execute();
+        $res = $statement->fetchAll();
+        /* $res = $statement->fetch(PDO::FETCH_OBJ); */
+        return $res;
+    }
 }
 
 ?>
