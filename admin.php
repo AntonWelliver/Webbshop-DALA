0b8a3e3a02c2e24fc2c3756289588339a4dffdd6
David Yan <daveddd97@hotmail.com>
2019-03-15 19:05:59 +0100

    Show if admin logged in, added link to admin page

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@ session_start();
    if (isset($_SESSION["user"])) {
       $username = $_SESSION["user"];
       if ($user->checkIfAdmin($username) != true) {
-         header("Location: http://example.com/myOtherPage.php");
+         header("Location: index.php");
          exit();
       } 
 
